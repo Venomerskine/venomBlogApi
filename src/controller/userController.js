406083a602e91c8ea63c71bf15be86360ac5227a
Lincoln Kubasu <kubasulincoln@gmail.com>
2026-05-03 09:46:18 +0300

    camelCase Error corection,

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -4,27 +4,35 @@ import prisma from '../../prisma/prismaClient.js';
 
 const register = async (req, res) => {
     const {firstName, lastName, email, password} = req.body;
+    console.log("Icoming body:", req.body);
+    console.log("Password type:", typeof password);
+    if (!password || typeof password !== "string") {
+    return res.status(400).json({ message: "Password is required" });
+}
 
     try {
         const existingUser = await prisma.user.findUnique({
             where: { email }
         });
+        console.log("Existing user check:", existingUser);
         if(existingUser) return res.status(400).json({ message: 'User already exists' });
 
         const hashedPassword = await bcrypt.hash(password, 10);
         
         const newUser = await prisma.user.create({
-            firstName,
-            lastName,
-            email,
-            password: hashedPassword
+            data: {
+                firstName,
+                lastName,
+                email,
+                password: hashedPassword
+            }
         });
 
         res.status(201).json({ message: 'User registered successfully' });  
     } catch (err) {
+        console.error("REGISTER ERROR:", err);
         res.status(500).json({ message: 'Error registering user' });
-
-    }
+}
 }
 
 export default {
