b895f5aef46ba349eb3ebd6c18090a1c32851b6f
Lincoln Kubasu <kubasulincoln@gmail.com>
2026-05-03 06:41:35 +0300

    created the prisma client, the register route

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,24 +1,24 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken';
+import prisma from '../../prisma/prismaClient.js';
 
 const register = async (req, res) => {
     const {firstName, lastName, email, password} = req.body;
 
     try {
-        const existingUser = await User.findOne({ email });
+        const existingUser = await prisma.user.findUnique({
+            where: { email }
+        });
         if(existingUser) return res.status(400).json({ message: 'User already exists' });
 
         const hashedPassword = await bcrypt.hash(password, 10);
         
-        const newUser = new User({
+        const newUser = await prisma.user.create({
             firstName,
             lastName,
             email,
             password: hashedPassword
         });
-
-        await newUser.save();
 
         res.status(201).json({ message: 'User registered successfully' });  
     } catch (err) {
@@ -27,6 +27,6 @@ const register = async (req, res) => {
     }
 }
 
-module.exports = {
+export default {
     register
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import userController from '../controller/userController.js'
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+router.post('/register', userController.register)
 
 export default router;
