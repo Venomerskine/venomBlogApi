451079e9e0ddf006a63880b448bb7c21ec5b2a61
Lincoln Kubasu <kubasulincoln@gmail.com>
2026-05-04 05:58:47 +0300

    comment

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -2,6 +2,7 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken';
 import prisma from '../../prisma/prismaClient.js';
 
+// Register a new user
 const register = async (req, res) => {
     const {firstName, lastName, email, password} = req.body;
     console.log("Icoming body:", req.body);
@@ -35,6 +36,8 @@ const register = async (req, res) => {
 }
 }
 
+
+// Login user and return JWT token
 const login = async (req, res) => {
     const {email, password} = req.body;
 
@@ -59,6 +62,7 @@ const login = async (req, res) => {
     res.json({message: "Login successful", token});
 }
 
+// Create a new post
 const createPost =  async (req, res) => {
     try {
             const {title, content, authorId} = req.body;
